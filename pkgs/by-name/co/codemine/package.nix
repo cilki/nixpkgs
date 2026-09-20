e1fab3192cc2d8475d989377187a3c4ca34fac1a
Tyler Cook <10459406+cilki@users.noreply.github.com>
2026-09-20 17:25:06 -0500

    wip: bump codemine

--- pkgs/by-name/co/codemine/package.nix
+++ pkgs/by-name/co/codemine/package.nix
@@ -6,19 +6,19 @@
 
 rustPlatform.buildRustPackage {
   pname = "codemine";
-  version = "0.1.0-unstable-2026-09-14";
+  version = "0.1.0";
 
   src = fetchFromGitHub {
     owner = "cilki";
     repo = "codemine";
-    rev = "9491a4ff8bcdd3f74868ce0a4b03267aec4116f0";
-    hash = "sha256-JWZoOEYeqGBtHDsm2NWdBFfObQlwFA3pm6ofytuiZ44=";
+    rev = "bb241c5f80c3771fd0a2712235f225e614dc3758";
+    hash = "sha256-Ko/H9pEAQipE0gTDjHQHddJ5or/W6B/s2uM0BWIkSfE=";
   };
 
-  cargoHash = "sha256-CXgHTwqDwKakHZ4vg+6IQcApQb7eCv8PxhNFZTvA9X8=";
+  cargoHash = "sha256-PvLGpktBYxD4F5cNkWAQTBiiA8bkNNfgkuAhj15UcoE=";
 
   meta = {
-    description = "Unattended software development harness that sweeps repositories with opencode turns";
+    description = "Turn AI agents into code-mining bots";
     homepage = "https://github.com/cilki/codemine";
     license = lib.licenses.unlicense;
     maintainers = with lib.maintainers; [ cilki ];
